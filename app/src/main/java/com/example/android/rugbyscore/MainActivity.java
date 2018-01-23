f48fbb9543e5585ccbe10152c450275663dc0153
ionelah <35505964+ionelah@users.noreply.github.com>
2018-01-23 13:47:14 +0200

    Add files via upload

--- app/src/main/java/com/example/android/rugbyscore/MainActivity.java
+++ app/src/main/java/com/example/android/rugbyscore/MainActivity.java
@@ -11,11 +11,15 @@ public class MainActivity extends AppCompatActivity {
 
     int pointsForA = 0;
     int pointsForB = 0;
+    TextView scoreViewA = null;
+    TextView scoreViewB = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreViewA = (TextView) findViewById(R.id.team_a_score);
+        scoreViewB = (TextView) findViewById(R.id.team_b_score);
     }
 
     @Override
@@ -76,15 +80,14 @@ public class MainActivity extends AppCompatActivity {
      * displays the given score for Team A
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewA.setText(String.valueOf(score));
     }
     /**
      * displays the given score for Team B
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
+
+        scoreViewB.setText(String.valueOf(score));
     }
 
     /**
